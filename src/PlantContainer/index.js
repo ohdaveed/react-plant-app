a79f40dc65b55cc29ffd8885282378c99f886547
David Arrizon <arrizon.david@gmail.com>
2019-11-14 16:42:26 -0600

    Connect react app to flask app

--- src/PlantContainer/index.js
+++ src/PlantContainer/index.js
@@ -28,12 +28,51 @@ class PlantContainer extends React.Component {
 			console.log(err);
 		}
 	};
+	addPlant = async (e, plantFromTheForm) => {
+		e.preventDefault();
+		console.log(plantFromTheForm);
+
+		// Post Request
+
+		try {
+			// We have to send JSON
+			// createdDogResponse variable will store the response from the express API
+			const createdPlantResponse = await fetch(
+				process.env.REACT_APP_API_URL + '/api/v1/plants/',
+				{
+					method: 'POST',
+					body: JSON.stringify(plantFromTheForm),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			);
+
+			// we have to turn the response from flask into
+			// an object we can use
+			const parsedResponse = await createdPlantResponse.json();
+			console.log(parsedResponse, ' this is response');
+
+			// we are emptying all the dogs that are living in state into a new array,
+			// and then adding the dog we just created to the end of it
+			// the new dog which is called parsedResponse.data
+
+			this.setState({
+				plants: [...this.state.plants, parsedResponse.data]
+			});
+		} catch (err) {
+			console.log('error');
+			console.log(err);
+		}
+		// request address will start with 'http://localhost:9000'
+		// because after we create it, we want to add it to the dogs array
+	};
 	render() {
 		return (
 			<React.Fragment>
 				<h2>Plants</h2>
 				<PlantList plants={this.state.plants} />
-				<CreatePlant />
+				<CreatePlant addPlant={this.addPlant} />
 			</React.Fragment>
 		);
 	}
