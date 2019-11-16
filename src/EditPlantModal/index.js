f6ba9c41eedc4c34859872989311ecbb388456b4
David Arrizon <arrizon.david@gmail.com>
2019-11-15 18:55:56 -0600

    modified npm modules

---
+++ src/EditPlantModal/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Form, Button, Label, Header, Modal} from 'semantic-ui-react';
+
+function EditPlantModal(props) {
+
+	return (
+		<Modal open={props.open} closeIcon onClose={props.closeModal})>
+		<Header>Edit Plant</Header>
+			<Modal.Content>
+
+
+}
